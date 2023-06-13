428f69e7997d6aefb127714b239c5179391e9ae3
Kietnhn <kietnguyen564042@gmail.com>
2023-06-13 17:10:43 +0700

    deploy

--- server/index.js
+++ server/index.js
@@ -36,6 +36,6 @@ app.use("/api/position", positionRouter);
 app.use("/api/air", airRouter);
 app.use("/api/sun", sunRouter);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
